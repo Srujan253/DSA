6443a2a61a00ae49fdf8c3d0406f12ddd80e3154
Srujan <srujanhm135@gmail.com>
2026-01-08 23:21:06 +0530

    refreshed basics of dsa and statred it using java

---
+++ javabasics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class javabasics{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("First programm for java : ");
+        float a=sc.nextFloat();
+        int b=(int)a;
+        System.out.println("The value of b is: "+b);
+        
+        sc.close();
+    }
+}
