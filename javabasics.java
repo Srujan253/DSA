4d8bd9216dc233f189bb90b107d038f03d12f2fb
Srujan <srujanhm135@gmail.com>
2026-01-09 09:18:20 +0530

    if else and loops practice in basic of java

--- javabasics.java
+++ javabasics.java
@@ -7,6 +7,13 @@ public class javabasics{
         float a=sc.nextFloat();
         int b=(int)a;
         System.out.println("The value of b is: "+b);
+        int age=22;
+        if(age>18){
+            System.out.println("You can drive and vote and etc");
+        }
+        else{
+            System.out.println("You cannot drive");
+        }
         
         sc.close();
     }
