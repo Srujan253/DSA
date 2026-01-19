808b13af2483e1a365f3ff962d087b5135428551
Srujan <srujanhm135@gmail.com>
2026-01-19 21:36:07 +0530

    functions are done

---
+++ functions.java
@@ -0,0 +1,12 @@
+public class functions{
+
+    public static int add(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+        int sum=add(a,b);
+        System.out.println("The sum of a and b is: "+sum);
+    }
+}
