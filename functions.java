b85b4ba3a09f1c5f4090460d9dfc7d3086387fc7
Srujan <srujanhm135@gmail.com>
2026-01-20 21:43:32 +0530

    factorial and fibonacci are done

--- functions.java
+++ functions.java
@@ -1,12 +1,48 @@
+import java.util.*;
+
 public class functions{
 
-    public static int add(int x, int y){
+    public  int add(int x, int y){
         return x+y;
     }
+
+    public int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }else{
+            return n*factorial(n-1);
+
+        }
+        }
+    public int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+
+        }
+        }
+    
+    
     public static void main(String[] args){
-        int a=5;
-        int b=10;
-        int sum=add(a,b);
-        System.out.println("The sum of a and b is: "+sum);
+        functions obj=new functions();
+        Scanner sc=new Scanner(System.in);
+        // int a=5;
+        // int b=10;
+        // int sum=obj.add(a,b);
+        // System.out.println("The sum of a and b is: "+sum);
+        System.out.print("Enter the number to find the factorial: ");
+        int num=sc.nextInt();
+        int fact=obj.factorial(num);
+        System.out.println("The factorial of "+num+" is: "+fact);
+
+        System.out.print("Enter the number to find the fibonacci: ");
+        int n=sc.nextInt();
+        int fib=obj.fibonacci(n);
+        System.out.println("The fibonacci of "+n+" is: "+fib);
+
     }
+
 }
