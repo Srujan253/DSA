28cff5d5e3ae4b29af0e1bd7d3ba8c34e2a7939c
Srujan <srujanhm135@gmail.com>
2026-05-29 12:19:47 +0530

    magic number and some basic integer operetions

--- python/day1/while_loop.py
+++ python/day1/while_loop.py
@@ -46,4 +46,22 @@ def reverse():
         rev=rev*10+remind
         num=num//10
     print(rev)
-reverse()
+# reverse()
+
+#magic number 
+def magic_number():
+    num=int(input("enter the number to check:"))
+    while num>9:
+        sum=0
+        while num>0:
+            remind=num%10
+            sum=sum+remind
+            num=num//10
+        num=sum
+    print(num)
+    if num==1:
+        print("magic number")
+    else:
+        print("not a magic number")
+magic_number()
+        
