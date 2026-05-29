cd92b31f0b10f4a8569d1d0e41370d19d4b531c2
Srujan <srujanhm135@gmail.com>
2026-05-29 15:55:58 +0530

    new patterns are done

--- python/day1/patter.py
+++ python/day1/patter.py
@@ -14,4 +14,17 @@ def below_or():
     n=int(input("enter the numbers :"))
     for i in range(n,0,-1):
         print("*"*i)
-below_or()
+# below_or()
+
+def only_border():
+    n=int(input("enter the number of stars to print:"))
+    k=1
+    for i in range(0,n):
+        for j in range(n):
+            if i==0 or j==0 or i==n-1 or j==n-1:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+                k=k+1
+        print()
+only_border()
