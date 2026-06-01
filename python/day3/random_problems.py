74fdaeae66e7982d13fd5497cf7b1531e8ee1294
Srujan <srujanhm135@gmail.com>
2026-06-01 13:11:33 +0530

    random  problems

--- python/day3/random_problems.py
+++ python/day3/random_problems.py
@@ -41,4 +41,38 @@ def missing_number(ls):
     n=len(ls)
     total=(n*(n+1))//2
     print(total-sum(ls))
-missing_number(ls)
+# missing_number(ls)
+
+#has to solveee
+# def encoded(ls):
+#     decode=[]
+#     for i in ls:
+
+
+#plus one
+def plus_one(ls):
+    # idx=len(ls)-1
+    # while idx>=0:
+    #     if idx==0 and ls[idx]==9:
+    #         ls.append(0)
+    #         ls[idx]=1
+    #         return ls 
+    #     elif ls[idx]==9:
+    #         ls[idx]=0
+    #         idx-=1
+    #     else:
+    #         ls[idx]+=1
+    #         idx=-1
+    # return ls
+    for i in range(len(ls)-1,-1,-1):
+        if ls[i]<9:
+            ls[i]+=1
+            return ls
+        else:
+            ls[i]=0
+    return [1]+ls
+
+print(plus_one(ls))
+       
+
+
