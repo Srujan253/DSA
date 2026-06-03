614b38e49c4535b14f7fbbede92b3db98ffe7964
Srujan <srujanhm135@gmail.com>
2026-06-03 12:06:06 +0530

    strings basic and operation without using inbuilt functions

---
+++ python/day5/Strings.py
@@ -0,0 +1,31 @@
+# .upper for upper case converstion
+#.isuppper for checking vice versa for lower case also
+#.index to find the index (find does not return error returns -1 but index does give error )
+#String is inmutable use replace indexing replacing does not work 
+def converstion_upper(s):
+    upper_s=""
+    for i in s:
+        
+        temp=ord(i)
+        text=i
+        if temp>=97:
+            temp=temp-32
+            text=chr(temp)
+        upper_s=upper_s+text
+    print(upper_s)
+def converstion_lower(s):
+    lower_s=""
+    for i in s:
+        
+        temp=ord(i)
+        text=i
+        if temp<=90:
+            temp=temp+32
+            text=chr(temp)
+        lower_s=lower_s+text
+    print(lower_s)
+
+converstion_upper("Neha__")
+converstion_lower("Neha__")
+
+
