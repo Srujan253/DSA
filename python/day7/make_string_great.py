d60eaaaa8924d2edb42c2a217a9dc980dcb75ac5
Srujan <srujanhm135@gmail.com>
2026-06-05 16:06:39 +0530

    last sessionending with summary

--- python/day7/make_string_great.py
+++ python/day7/make_string_great.py
@@ -1,7 +1,7 @@
 def great_string(s):
     res=[]
     for ch in s:
-        if res and ch.lower!= res[-1].lower and res[-1]==ch:
+        if res and ch.lower()!= res[-1].lower() and res[-1]==ch:
             res.pop()
         else:
             res.append(ch)
