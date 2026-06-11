454744dc0f5f731bb82c905d718994944c8ae5ed
Srujan <srujanhm135@gmail.com>
2026-06-11 14:55:26 +0530

    dfs tree are doen

--- python/day12/dfs.py
+++ python/day12/dfs.py
@@ -1,11 +1,10 @@
-def dfs(graph,node):
-    visited=set()
-    def dfs_helper(graph,node):
-        if node not in visited:
-            print(node)
-            visited.add(node)
-            for neighbor in graph[node]:
-                dfs_helper(neighbor)
-    dfs_helper(graph,node)
-graph={'A':['B','C'],'B':['A','D','E'],'C':['A','F'],'D':['B'],'E':['B','F'],'F':['C','E']}
-dfs(graph,'A')
+visited=set()
+def dfs_helper(graph,node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs_helper(graph, neighbor)
+    
+graph={'A':['B','C'],'B':['A','D'],'C':['A','D'],'D':['B','C','E'],'E':['B','D']}
+dfs_helper(graph,'A')
